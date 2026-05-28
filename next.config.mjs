bf8e5fd17fb5b2e5c2fc7a85d361173e13ec2085
Star Mdakane <mmdakane@rocketmail.com>
2026-05-28 20:17:35 +0200

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
 /** @type {import('next').NextConfig} */
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    resolveAlias: {
+      root: path.join(__dirname, '..'),
+    },
+  },
 };
+
 
 export default nextConfig;
