031c367fe69294893fe46b40217dea8af1ebbd06
Star Mdakane <mmdakane@rocketmail.com>
2026-05-29 18:10:45 +0200

    forms...

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,6 +1,22 @@
+import Link from "next/link";
+
 const LoginPage = () => {
     return (
-        <div>LoginPage</div>
+        <form>
+            <h1>Login</h1>
+            <div>
+                <div>
+                    Email
+                </div>
+                <div>
+                    Password
+                </div>
+            </div>
+            <div>
+                <button type="submit">Login to your account</button>
+                <p> Don't have an account? <Link href="/app/register">Sign Up</Link></p>
+            </div>
+        </form>
     )
 }
 
